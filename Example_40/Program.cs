4493cdfc988c963fa3d4e0a7a128493e965ce327
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-28 13:55:22 +0300

    задача 40 решена

--- Example_40/Program.cs
+++ Example_40/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: 
+// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+Console.Write("Введите первое число: ");
+int numFirst = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numSecond = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numThird = int.Parse(Console.ReadLine());
+
+if (numFirst < numSecond + numThird && numSecond < numFirst + numThird && numThird < numFirst + numSecond)
+{
+    Console.Write("Может существовать");
+}
+else
+{
+    Console.Write("Не может существовать");
+}
+
