167e66518fba24efdeb5c93a1489ee0009be69d0
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-28 17:40:10 +0300

    Задача 42 решена

--- Example_42/Program.cs
+++ Example_42/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 42: 
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string result = string.Empty;
+int step = 0;
+while (number != 0)
+{
+    step = number % 2;
+    number /= 2;
+    result = step + result;
+}
+
+Console.Write("В двоичной системе число равно: " + result);
