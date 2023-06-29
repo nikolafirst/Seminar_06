412522aa4c1767b9ceaca850241faad9830d2dfb
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-29 10:42:48 +0300

    Дано решение для задачи 43

---
+++ DZ_Task_43/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+// Создаю строковую переменную Aui - all user input для сбора данных и Ui - user input для ввода
+string Aui = string.Empty;
+string Ui = string.Empty;
+
+// Создаю запрос на ввод в цикле до ввода 4 значений
+Console.Clear();
+while(Aui.Length < 4){
+    Console.Write("Введите " + Aui.Length + " число : ");
+    Ui = Console.ReadLine();
+    Aui += Ui;
+}
+
+Console.Write("Точка пересечения двух прямых : ");
+int x = 0;
+int y = 0;
+while(Aui[1] * x + Aui[0] != y){
+    x++;
+    while(Aui[3] * x + Aui[2] = y){
+        y++;
+    }
+}
